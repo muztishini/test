e6f07f280e52c3fd684754c18ee35f9ab0d8d806
muztishini <a.kroshakov@yandex.ru>
2022-08-20 14:59:14 +0500

    fg

---
+++ test.py
@@ -0,0 +1,12 @@
+n = int(input())
+m = float(input())
+if n == 1:
+    print('kg = ', m)
+elif n == 2:
+    print('kg = ', m / 1000000)
+elif n == 3:
+    print('kg = ', m / 1000)
+elif n == 4:
+    print('kg = ', m * 1000)
+elif n == 5:
+    print('kg = ', m * 100000)
